c3faf3c3e23a28cccb40eabca2801ae78160063c
soi-ha <soy2302ten@gmail.com>
2023-07-31 00:44:59 +0900

    ButtonSecelct 틀 구현(디자인 요청중/수정예정)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Reset } from 'styled-reset';
 import './App.scss';
 import Button from './components/Button';
+import ButtonSelect from './components/ButtonSelect';
 import Img from './assets/images/save.png';
 
 function App() {
@@ -8,6 +9,11 @@ function App() {
 		<div className="App">
 			<Reset />
 			<Button img={Img} content={'업로드'} type={'acting'} />
+			<ButtonSelect>
+				<option value="hi">jdddddddo</option>
+				<option value="hi">jo</option>
+				<option value="hi">jo</option>
+			</ButtonSelect>
 		</div>
 	);
 }
