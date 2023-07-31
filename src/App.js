6958ed4e995d6603717fb109920c3677d2216a2d
soi-ha <soy2302ten@gmail.com>
2023-07-31 16:09:05 +0900

    svg파일로 변경 및 버튼(Button) 구현 완료

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { Reset } from 'styled-reset';
 import './App.scss';
 import Button from './components/Button';
 import ButtonSelect from './components/ButtonSelect';
-import Img from './assets/images/save.png';
+import Img from './assets/images/right-btn.svg';
 
 function App() {
 	return (
 		<div className="App">
 			<Reset />
-			<Button img={Img} content={'업로드'} type={'acting'} />
+			<Button img={Img} content={'더보기'} type={'acting'} />
 			<ButtonSelect>
 				<option value="hi">jdddddddo</option>
 				<option value="hi">jo</option>
