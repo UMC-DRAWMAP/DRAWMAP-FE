e53d322770fcacdbc4373cd7c707c1dd958ccfd1
soi-ha <soy2302ten@gmail.com>
2023-07-31 18:39:46 +0900

    SmallDrawBox 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Reset } from 'styled-reset';
 import './App.scss';
+import SmallDrawBox from './components/SmallDrawBox';
 
 function App() {
 	return (
 		<div className="App">
 			<Reset />
+			<SmallDrawBox />
 		</div>
 	);
 }
