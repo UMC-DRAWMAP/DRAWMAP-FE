163591ef883bdb72ae2357cdc2e9d3c49ea18b6e
YangCaptain <ekqlsdlrns@naver.com>
2023-08-02 13:28:14 +0900

    ArchiveBox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Reset } from 'styled-reset';
 import './App.scss';
+import ArchiveBox from './components/ArchiveBox';
 
 function App() {
 	return (
 		<div className="App">
 			<Reset />
+			<ArchiveBox/>
 		</div>
 	);
 }
