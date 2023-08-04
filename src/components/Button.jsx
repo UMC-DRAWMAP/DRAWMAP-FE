01ba088d74b0d88819b46df622beb0950551ae6b
jungwoo3490 <jungwoo3490@naver.com>
2023-08-04 21:39:25 +0900

    버튼 이벤트 핸들러 prop 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import './Button.scss';
 
-export default function Button({ img, type, content }) {
+export default function Button({ img, type, content, onClick }) {
 	return (
-		<button className={`btn ${type}`}>
+		<button className={`btn ${type}`} onClick={onClick}>
 			<img src={img} alt="button img" style={{ display: img ? 'block' : 'none' }} />
 			<span style={{ display: img ? 'none' : 'block' }}>{content}</span>
 		</button>
