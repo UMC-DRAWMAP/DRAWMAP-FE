d8775025df4bc3645d22de25899cbd299d438a71
soi-ha <soy2302ten@gmail.com>
2023-08-14 16:33:32 +0900

    버튼 type=default 사이즈 변경

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { Reset } from 'styled-reset';
 import './App.scss';
 import Button from './components/Button';
 import ButtonSelect from './components/ButtonSelect';
-import Img from './assets/images/arrow-right.svg';
+import Img from './assets/images/save.svg';
 
 function App() {
 	return (
 		<div className="App">
 			<Reset />
-			<Button img={Img} content={'더보기'} type={'acting'} />
+			<Button img={Img} content={'더보기'} type={'default'} />
 			<ButtonSelect>
 				<option value="hi">jdddddddo</option>
 				<option value="hi">jo</option>
