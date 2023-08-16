4aa3b313f86231782498254023023efee8df03ab
soi-ha <soy2302ten@gmail.com>
2023-08-16 23:56:44 +0900

    데이터 값 뿌리기 구현중

--- src/components/ButtonSelect.jsx
+++ src/components/ButtonSelect.jsx
@@ -2,8 +2,87 @@ import './ButtonSelect.scss';
 import DropBtn from '../assets/images/dropdown-btn.svg';
 import { useState } from 'react';
 
+const dataList = [
+	{
+		item: '서울',
+	},
+	{
+		item: '경기',
+	},
+	{
+		item: '인천',
+	},
+	{
+		item: '대전',
+	},
+	{
+		item: '광주',
+	},
+	{
+		item: '울산',
+	},
+	{
+		item: '부산',
+	},
+	{
+		item: '대구',
+	},
+	{
+		item: '제주',
+	},
+	{
+		item: '강원',
+	},
+	{
+		item: '충청',
+	},
+];
+// const dataList = {
+// 	1: {
+// 		item: '서울',
+// 	},
+// 	2: {
+// 		item: '경기',
+// 	},
+// 	3: {
+// 		item: '인천',
+// 	},
+// 	4: {
+// 		item: '대전',
+// 	},
+// 	5: {
+// 		item: '광주',
+// 	},
+// 	6: {
+// 		item: '울산',
+// 	},
+// 	7: {
+// 		item: '부산',
+// 	},
+// 	8: {
+// 		item: '대구',
+// 	},
+// 	9: {
+// 		item: '제주',
+// 	},
+// 	10: {
+// 		item: '강원',
+// 	},
+// 	11: {
+// 		item: '충청',
+// 	},
+// };
+
 export default function ButtonSelect() {
 	const [isActive, setIsActive] = useState(false);
+
+	// console.log(Object.keys(dataList));
+	// json 형태일 때 값 불러오기
+	// for (const key in Object.keys(dataList)) {
+	// 	let li = `<li>${dataList[key]}</li>`;
+	// 	// console.log(dataList[key]);
+	// 	console.log(li);
+	// }
 
 	return (
 		// name : select name
@@ -27,7 +106,10 @@ export default function ButtonSelect() {
 			</div>
 			<div className="sbtn-content" style={{ display: isActive ? 'block' : 'none' }}>
 				<ul className="sbtn-options">
-					<li>서울특별시장안구우우루우ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ</li>
+					{dataList.map((it) => {
+						<li>{it.value}</li>;
+					})}
+					{/* <li>서울특별시장안구우우루우ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ</li>
 					<li>경기</li>
 					<li>인천</li>
 					<li>대전</li>
@@ -51,7 +133,7 @@ export default function ButtonSelect() {
 					<li>경기</li>
 					<li>인천</li>
 					<li>대전</li>
-					<li>부산</li>
+					<li>부산</li> */}
 				</ul>
 			</div>
 		</div>
