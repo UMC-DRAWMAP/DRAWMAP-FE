12f218dde46a0208ecd61247988cdc802d539f4b
jungwoo3490 <jungwoo3490@naver.com>
2023-08-20 19:03:03 +0900

    DrawBox 컴포넌트 수정

--- src/components/DrawBox.jsx
+++ src/components/DrawBox.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import SmallDrawBox from './SmallDrawBox';
 import './DrawBox.scss';
+import DefaultProfile from '../assets/images/profile-mini.svg';
+import Stars from './Stars';
 
-export default function DrawBox() {
+
+
+export default function DrawBox({ img, userName, userProfile, satisfaction, review }) {
   return (
-    <div className="draw-box">
-      <SmallDrawBox />
-      <div className="draw-content">
-        <span className="draw-text" style={{ left: '25.56px', top: '30.05px', width: '92.55px', height: '21.7px' }}>
-          만족도
-        </span>
-        <span className="draw-rating" style={{ left: '25.56px', top: '50.99px', width: '101.03px', height: '15.43px' }}>
-          ★★★☆☆
-        </span>
-        <span className="draw-review" style={{ left: '25.56px', top: '70.66px', width: '248.31px', height: '85.33px' }}>
-          강아지 그림이 나와서 너무 재밌어요 또 타고 싶어요 다른 분들한테도 추천할 예정이에요.
-        </span>
+    <div className="DrawBox">
+      <div className='imagecontainer'>
+        <img src={img} alt="" />
+        <div className='nicknamebox'>
+          <p className='nickname'>{userName}</p>
+          <img
+            src={userProfile ? userProfile : DefaultProfile}
+            alt="유저 프로필 이미지"
+            className="sdb-user-profile-img"
+				  />
+        </div>
       </div>
+      <div className='reviewcontainer'>
+        <div className='satisfactioncontainer'>
+          <p className='satisfaction'>만족도</p>
+          <Stars n={satisfaction}/>
+        </div>
+        <div className='reviewtext'>
+          {review}
+        </div>
+      </div>      
     </div>
   );
-}
+};
 
