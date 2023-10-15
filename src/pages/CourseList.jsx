48798530b2ba1a82107c2a59d1eb2e7246dc3070
soi-ha <soy2302ten@gmail.com>
2023-10-15 16:05:05 +0900

    cl-location display 변경하기

--- src/pages/CourseList.jsx
+++ src/pages/CourseList.jsx
@@ -6,7 +6,8 @@ import CourseListView from '../components/CourseListView';
 import PencilImg from '../assets/images/pencil.svg';
 import XBtn from '../assets/images/x-btn.svg';
 import ButtonSelect from '../components/ButtonSelect';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function CourseList() {
 	const [textValue, setTextValue] = useState('');
@@ -14,6 +15,19 @@ export default function CourseList() {
 	const getTextValue = (text) => {
 		setTextValue(text);
 	};
+
+	// const jwtToken = 'soha';
+	useEffect(() => {
+		axios({
+			// headers: {
+			// 	'x-access-token': jwtToken,
+			// },
+			method: 'get',
+			url: 'http://54.80.0.204:9000/usercourse',
+		}).then((res) => {
+			console.log(res);
+		});
+	}, []);
 
 	return (
 		<div className="course-list">
@@ -23,8 +37,8 @@ export default function CourseList() {
 				<Button type={'text-btn'} content={'인기순'} />
 				<Button type={'text-btn'} content={'최신순'} />
 				<ButtonSelect title={'지역별'} getTextValue={getTextValue} textValue={textValue} />
-				{getTextValue}
-				<div className="cl-location">
+				{/* {getTextValue} */}
+				<div className="cl-location" style={{ display: textValue ? 'inline-flex' : 'none' }}>
 					{textValue}
 					<img src={XBtn} alt="삭제 버튼" className="cl-x-btn" />
 				</div>
